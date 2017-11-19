2a33d754c109938fde76a2d6f5672efacd34bcb5
Alan <a541203951@163.com>
2017-11-19 21:30:36 +0800

    fix bug

--- phone_captcha/func/sms.py
+++ phone_captcha/func/sms.py
@@ -2,8 +2,8 @@ import json
 import requests
 
 header = {
-    "X-LC-Id": 4cROByWxr8AfwE8KXoHlCnI6-gzGzoHsz,
-    "X-LC-Key": q3LYtpmW69TNLIM0qkO2mFMD,
+    "X-LC-Id": '4cROByWxr8AfwE8KXoHlCnI6-gzGzoHsz',
+    "X-LC-Key": 'q3LYtpmW69TNLIM0qkO2mFMD',
     "Content-Type": "application/json",
 }
 
