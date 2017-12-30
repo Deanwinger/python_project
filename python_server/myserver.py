336e06a74d7ea78d677ec8d15c045f1c9126c79d
Alan <a541203951@163.com>
2017-12-30 21:12:17 +0800

    finished first batch

--- python_server/myserver.py
+++ python_server/myserver.py
@@ -2,6 +2,13 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 import os, sys
 import subprocess
 
+# os.name字符串指示你正在使用的平台。比如对于Windows，它是’nt’，而对于Linux/Unix用户，它是’posix’
+if os.name == 'nt':
+    my_work_directory = 'D:\python-related\python_server'
+else:
+    my_work_directory = '/home/ubuntu/alan/python_related/python_fundemental'
+
+
 #-------------------------------------------------------------------------------
 
 class ServerException(Exception):
@@ -10,7 +17,28 @@ class ServerException(Exception):
 
 #-------------------------------------------------------------------------------
 
-class case_no_file(object):
+class base_case(object):
+    def handle_file(self, handler, full_path):
+        try:
+            with open(full_path, 'rb') as reader:
+                content = reader.read()
+            handler.send_content(content)
+        except IOError as msg:
+            msg = "'{0}' cannot be read: {1}".format(full_path, msg)
+            handler.handle_error(msg)
+
+    def index_path(self, handler):
+        return os.path.join(handler.full_path, 'index.html')
+
+    def test(self, handler):
+        assert False, 'Not implemented.'
+    
+    def act(self, handler):
+        assert False, 'Not implemented.'
+
+#-------------------------------------------------------------------------------
+
+class case_no_file(base_case):
     def test(self, handler):
         return not os.path.exists(handler.full_path)
 
@@ -19,39 +47,42 @@ class case_no_file(object):
 
 #-------------------------------------------------------------------------------
 
-class case_existing_file(object):
+class case_existing_file(base_case):
     def test(self, handler):
         return os.path.isfile(handler.full_path)
 
     def act(self, handler):
-        handler.handle_file(handler.full_path)
+        self.handle_file(handler, handler.full_path)
 
 #-------------------------------------------------------------------------------
 
-class case_directory_index_file(object):
-    def index_path(self, handler):
-        return os.path.join(handler.full_path, 'index.html')
-
+class case_directory_index_file(base_case):
     def test(self, handler):
         return os.path.isdir(handler.full_path) and \
                os.path.isfile(self.index_path(handler))
 
     def act(self, handler):
-        handler.handle_file(self.index_path(handler))
+        self.handle_file(handler, self.index_path(handler))
 
 #-------------------------------------------------------------------------------
 
-class case_cgi_file(object):
+class case_cgi_file(base_case):
     def test(self, handler):
         return os.path.isfile(handler.full_path) and \
                     handler.full_path.endswith('.py')
 
     def act(self, handler):
-        handler.run_cgi(handler.full_path)
+        self.run_cgi(handler)
+
+    def run_cgi(self, handler):
+        cmd = "python " + handler.full_path
+        p = subprocess.Popen(cmd, shell=False, stdout=subprocess.PIPE)
+        data = p.stdout.read()
+        handler.send_content(data)
 
 #-------------------------------------------------------------------------------
 
-class case_always_fail(object):
+class case_always_fail(base_case):
     def test(self, handler):
         return True
 
@@ -81,22 +112,12 @@ class RequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         try:
             self.full_path = os.getcwd() + self.path
-
             for case in self.Cases:
                 if case.test(self):
                     case.act(self)
                     break
         except Exception as e:
             self.handle_error(e)
-
-    def handle_file(self, full_path):
-        try:
-            with open(full_path, 'rb') as reader:
-                content = reader.read()
-            self.send_content(content)
-        except IOError as msg:
-            msg = "'{0}' cannot be read: {1}".format(self.path, msg)
-            self.handle_error(msg)
     
     def handle_error(self, msg):
         content = self.Error_Page.format(path=self.path, msg=msg)
@@ -108,19 +129,6 @@ class RequestHandler(BaseHTTPRequestHandler):
         self.send_header("Content-Length", str(len(content)))
         self.end_headers()
         self.wfile.write(content)
-
-    def run_cgi(self, full_path):
-        cmd = "python " + full_path
-
-
-
-
-
-
-
-
-
-
 
 
 
