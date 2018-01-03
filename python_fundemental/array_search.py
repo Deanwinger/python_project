7bb0baecd6487fade0a016f132fffaed78c48777
Alan <a541203951@163.com>
2018-01-03 22:01:38 +0800

    题目3

---
+++ python_fundemental/array_search.py
@@ -0,0 +1,63 @@
+'''
+在一个二维数组中，每一行都按照从左到右递增的顺序排序
+每一列都按照从上到下递增的顺序排序。
+请完成一个函数，输入这样的一个二维数组和一个整数，判断数组中是否含有该整数。
+'''
+
+'''
+例子：
+1  2  8   9
+2  4  9   12
+4  7  10  13
+6  8  11  15
+以右上角第一个数（良好的性质， 比下小， 比左大）为参考
+'''
+
+def find_exact_one(matrix, target):
+    #情形一：matrix=[]
+    if not matrix:
+        return False
+
+    #类型判断和数据结构完整性判断
+    rows = len(matrix)
+    cols = len(matrix[0])
+
+    r_rec = 0
+    c_rec = len(matrix[0])-1
+
+    while r_rec < rows and c_rec >= 0:
+        if matrix[r_rec][c_rec] == target:
+            return True
+        elif matrix[r_rec][c_rec] > target:
+            c_rec -= 1
+        else:
+            r_rec += 1
+    return False
+
+if __name__ == '__main__':
+    array = [
+                [1, 2, 8, 9],
+                [2, 4, 9, 12],
+                [4, 7, 10, 13],
+                [6, 8, 11, 15],
+            ]
+    array2 = []
+    array3 = [
+                ['a', 'b', 'c'],
+                ['b', 'c', 'd'],
+            ]
+    array4 = [
+                [62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80],
+                [63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81],
+                [64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82],
+                [65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83],
+                [66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84],
+                [67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85],
+            ]
+
+    print(find_exact_one(array, 5))
+    print(find_exact_one(array, 10))
+    print(find_exact_one(array, 15))
+    print(find_exact_one(array2, 10))
+    print(find_exact_one(array3, 'b'))
+    print(find_exact_one(array4, 81))
