4892fe841b17fb7b5147a3e024cd87b2736fecb4
liuxiaoguang <vincexgliu@gamil.com>
2018-01-18 11:28:12 +0800

    题6 finished

--- python_fundemental/btree_rebuild.py
+++ python_fundemental/btree_rebuild.py
@@ -3,7 +3,7 @@
 假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
 例如输入前序遍历序列{1,2,4,7,3,5,6,8}和中序遍历序列{4,7,2,1,5,3,8,6}，则重建二叉树并返回。
 '''
-
+# leetcode 105
 #关键在于递归遍历， 关键点就在于找到根节点， 然后对于每个子树，都用找根节点确定左右子树的方式递归的进行下去
 
 class Node(object):
@@ -12,27 +12,55 @@ class Node(object):
         self.left = None
         self.right = None
 
-def btree_rebuild(pre_list, mid_list):
-    root = Node(pre_list[0])
-    if pre_list == [] and mid_list == []:
-        return Node(pre_list[0])
-    for i in range(len(mid_list)):
-        if pre_list[0] == mid_list[i]:
-            root.left = Node(pre_list[1])
-            root.right = Node(pre_list[i+1])
+# bug版
+# def btree_rebuild(pre_list, mid_list):
+#     if (not pre_list) or (not mid_list):
+#         return None
 
-            l_pre_list = pre_list[1:i+1]
-            l_mid_list = mid_list[:i]
-            left_tree = btree_rebuild(pre_list, mid_list)
+#     root = Node(pre_list[0])
+#     if len(pre_list) == 1:
+#         return 
+#     for i in range(len(mid_list)):
+#         if root == mid_list[0]:
+#             #只有右子树
+#             root.right = Node(pre_list[i+1])
+#             btree_rebuild(pre_list[1:], mid_list[1:])
+#         elif root == mid_list[len(pre_list)-1]:
+#             #只有左子树
+#             root.left = Node(pre_list[1])
+#             btree_rebuild(pre_list[1:], mid_list[:len(pre_list)-1])
+#         else:
+#             #左右都有
+#             root.left = Node(pre_list[1])
+#             root.right = Node(pre_list[i+1])
 
-            r_pre_list = pre_list[i+1:]
-            r_mid_list = mid_list[i+1:]
-            right_tree = btree_rebuild(pre_list, mid_list)
+#             l_pre_list = pre_list[1:i+1]
+#             l_mid_list = mid_list[:i]
+#             left_tree = btree_rebuild(pre_list, mid_list)
 
+#             r_pre_list = pre_list[i+1:]
+#             r_mid_list = mid_list[i+1:]
+#             right_tree = btree_rebuild(pre_list, mid_list)
+    
+#     return pre_list[0]
+
+def reConstructBinaryTree(pre, tin):
+    if not pre and not tin:
+        return None
+
+    if set(pre) != set(tin):
+        return None
+
+    root = Node(pre[0])
+    i = tin.index(pre[0])
+    root.left = reConstructBinaryTree(pre[1:i+1], tin[:i])
+    root.right = reConstructBinaryTree(pre[i+1:], tin[i+1:])
+    return root
+        
 
     
 if __name__ == '__main__':
     pre = [1, 2, 3, 5, 6, 4]
     tin = [5, 3, 6, 2, 4, 1]
-    print(btree_rebuild(pre, tin))
+    print(reConstructBinaryTree(pre, tin))
 
