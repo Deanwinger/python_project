42ae4f3bb4f41babdac2a38e6a86671271e66c98
Alan <a541203951@163.com>
2018-01-20 10:57:08 +0800

    题11 finished

--- python_fundemental/int_exponential.py
+++ python_fundemental/int_exponential.py
@@ -12,9 +12,30 @@
 '''
 当n为偶数, a^n = a^(n/2) * a^(n/2)
 当n为奇数, a^n = a^((n-1)/2) * a^((n-1)/2)) * a
-利用右移一位运算代替除以2
+利用右移一位运算代替除以2(类似于整除//)
 利用位与运算代替了求余运算法%来判断一个数是奇数还是偶数
 优化代码速度
 '''
-
+#leetcode 题50
 #题11
+
+class Solution(object):
+    def Power(self, base, exponential):
+        if base == 0:
+            if exponential == 0:
+                raise Exception("没有意义")
+            else:
+                return 0
+        
+        if exponential == 0:
+            return 1
+        if exponential == 1:
+            return base
+        if exponential == -1:
+            return 1/base
+        
+        result = self.Power(base, exponential>>1)
+        result *= result
+        if exponential%2 == 1:
+            result *= base 
+        return result
