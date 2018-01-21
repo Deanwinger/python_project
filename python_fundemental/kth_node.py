f384f95a9449e69dd0edd1bd58dc9171b864f122
Alan <a541203951@163.com>
2018-01-21 17:25:01 +0800

    题15 finished

--- python_fundemental/kth_node.py
+++ python_fundemental/kth_node.py
@@ -10,3 +10,40 @@
 当地一个指针指向尾节点的时候, 第二个指针正好指向倒数第k个结点
 推广: 寻找中间节点, 两个指针一起, 第一个指针每次走两步, 第二个指针每次走一步,  快指针指到尾部, 慢指针正好指到中间
 '''
+
+#leetcode 19. Remove Nth Node From End of List
+#题15
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution(object):
+    def findKthNode(self, head, k):
+        if k <= 0:
+            return
+        slow = head
+        n=0
+        while k-1 > n:
+            #先走后记录
+            head = head.next
+            n += 1
+            if head.next is None:
+                return
+        while head.next is not None:
+            head = head.next
+            slow = slow.next
+        return slow
+
+
+if __name__=='__main__':
+    node1 = ListNode(10)
+    node2 = ListNode(11)
+    node3 = ListNode(13)
+    node1.next = node2
+    node2.next = node3
+
+    S = Solution()
+    print(S.findKthNode(node1, 1).val)
