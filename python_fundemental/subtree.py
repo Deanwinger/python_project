ff0d761354acaee753487b3e53dcbd8c5e0a4106
liuxiaoguang <vincexgliu@gamil.com>
2018-01-23 10:29:25 +0800

    题18 finished

--- python_fundemental/subtree.py
+++ python_fundemental/subtree.py
@@ -46,4 +46,25 @@ class Solution(object):
 
 
 if __name__ == '__main__':
-    node1 = TreeNode(1)
+    pRoot1 = TreeNode(8)
+    pRoot2 = TreeNode(8)
+    pRoot3 = TreeNode(7)
+    pRoot4 = TreeNode(9)
+    pRoot5 = TreeNode(2)
+    pRoot6 = TreeNode(4)
+    pRoot7 = TreeNode(7)
+    pRoot1.left = pRoot2
+    pRoot1.right = pRoot3
+    pRoot2.left = pRoot4
+    pRoot2.right = pRoot5
+    pRoot5.left = pRoot6
+    pRoot5.right = pRoot7
+
+    pRoot8 = TreeNode(8)
+    pRoot9 = TreeNode(9)
+    pRoot10 = TreeNode(2)
+    pRoot8.left = pRoot9
+    pRoot8.right = pRoot10
+
+    S = Solution()
+    print(S.find_subtree(pRoot1, pRoot8))
