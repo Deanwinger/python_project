ac2abf9fc443eb037c5c61dc3ef5047c3fb4b8c4
Alan <a541203951@163.com>
2018-01-27 20:47:09 +0800

    题25

--- python_fundemental/path_sum.py
+++ python_fundemental/path_sum.py
@@ -2,3 +2,28 @@
 输入一颗二叉树和一个整数，打印出二叉树中结点值的和为输入整数的所有路径。
 路径定义为从树的根结点开始往下一直到叶结点所经过的结点形成一条路径。
 '''
+
+#leetcode 113. Path Sum II, 类似还有112. Path Sum 和 437. Path Sum III
+#题25
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def pathSum(self, root, sum):
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: List[List[int]]
+        """
+        if root.left is None and root.right is None:
+            #叶子节点
+            if 
+
+
+if __name__=='__main__':
+    pass
