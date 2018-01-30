c31a6f65cd7cdc0c5f8583b67be34e04368e9546
liuxiaoguang <vincexgliu@gamil.com>
2018-01-30 17:08:21 +0800

    题35 to be finished

--- python_fundemental/first_appear.py
+++ python_fundemental/first_appear.py
@@ -1,3 +1,15 @@
 '''
 在一个字符串(1<=字符串长度<=10000，全部由大写字母组成)中找到第一个只出现一次的字符。
 '''
+
+class Solution(object):
+    def firstAppear(self, array):
+        pass
+
+if __name__=='__main__':
+    # array = [5, 7, 6, 9, 11, 10, 8]
+    # array = [7, 4, 6, 5]
+    array = [4, 6, 7, 5]
+    # array3 = [1, 2, 3, 4, 5]
+    S = Solution()
+    print(S.firstAppear(array))
