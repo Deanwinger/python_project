9c98f9a621721758b2a65e3081b998dc3e364620
liuxiaoguang <vincexgliu@gamil.com>
2018-01-30 15:49:02 +0800

    题31 finished

--- python_fundemental/max_sum_of_subarray.py
+++ python_fundemental/max_sum_of_subarray.py
@@ -6,30 +6,31 @@
 
 
 #leetcode 53. Maximum Subarray
-#题31
+#题31 教训
 
 
 class Solution(object):
     def maxSum(self, array):
         if not array:
             return 
-        rec_sum = sum = 0
-        i = 0
-        while i < len(array):
-            temp = sum
-            sum += array[i]
-            if temp < sum:
-                rec_sum = sum
-            if sum < 0:
-                sum = 0
-            i += 1
-        return rec_sum
-            
+        nCurSum = 0
+        nGreatestSum = array[0]
+        for i in range(len(array)):
+            if nCurSum <= 0:
+                nCurSum = array[i]
+            else:
+                nCurSum += array[i]
 
+            if nCurSum > nGreatestSum:
+                nGreatestSum = nCurSum
+
+        return nGreatestSum
+    
 
 
 if __name__=='__main__':
+    array = [-2,1,-3,4,-1,2,1,-5,4]
     # array = [1, 2, 3, 2, 2, 2, 5, 4, 2]
-    array = [1, -2, 3, 10, -4, 7, 2, -5]
+    # array = [1, -2, 3, 10, -4, 7, 2, -5]
     s = Solution()
     print(s.maxSum(array))
