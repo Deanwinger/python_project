a2c43eee0b78a4e9eb8a3886f5b3b6e7120e4741
liuxiaoguang <vincexgliu@gamil.com>
2018-01-31 12:06:09 +0800

    题38 to be finished

--- python_fundemental/count_num.py
+++ python_fundemental/count_num.py
@@ -1,3 +1,25 @@
 '''
 统计一个数字在排序数组中出现的次数。
 '''
+
+#题38
+#leetcode 类似题34. Search for a Range
+
+class Solution:
+    def GetNumberOfK(self, data, k):
+        start = 0
+        end = len(data) - 1
+        mid = (start + end) // 2
+        if data[mid] > k:
+            pass
+        elif data[mid] < k:
+            pass
+        else data[mid] == k:
+            self.GetFirstK()
+            self.GetLastK()
+
+    def GetFirstK(self, data, length, k, start, end):
+        pass
+
+    def GetLastK(self, data, length, k, start, end):
+        pass
