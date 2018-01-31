ee6a16e168be5c4af0e189ecc71b20c2cca617c8
liuxiaoguang <vincexgliu@gamil.com>
2018-01-31 16:17:06 +0800

    题38 finished

--- python_fundemental/count_num.py
+++ python_fundemental/count_num.py
@@ -7,19 +7,46 @@
 
 class Solution:
     def GetNumberOfK(self, data, k):
+        if not data:
+            return
+        number = 0
         start = 0
-        end = len(data) - 1
+        end = len(data) - 1 
+        first = self.GetFirstK(data, k, start, end)
+        last = self.GetLastK(data, k, start, end)
+        if first > -1:
+                number = last - first + 1
+        return number
+
+    def GetFirstK(self, data, k, start, end):
+        if start > end:
+            return -1
         mid = (start + end) // 2
-        if data[mid] > k:
-            pass
-        elif data[mid] < k:
-            pass
-        else data[mid] == k:
-            self.GetFirstK()
-            self.GetLastK()
+        if data[mid] == k:
+            if mid > 0 and data[mid-1] == k:
+                end = mid-1
+            else:
+                return mid
+        elif data[mid] > k:
+            end = mid - 1
+        else:
+            start = mid + 1
+        return self.GetFirstK(data, k, start, end)
 
-    def GetFirstK(self, data, length, k, start, end):
-        pass
+    def GetLastK(self, data, k, start, end):
+        mid = (start + end) // 2
+        if data[mid] == k:
+            if mid < end and data[mid+1] == k:
+                start = mid + 1
+            else:
+                return mid
+        elif data[mid] > k:
+            end = mid - 1
+        else:
+            start = mid + 1
+        return self.GetLastK(data, k, start, end)
 
-    def GetLastK(self, data, length, k, start, end):
-        pass
+if __name__=='__main__':
+    alist = [3,3,3,3,4,5]
+    s = Solution()
+    print(s.GetNumberOfK(alist, 3))
