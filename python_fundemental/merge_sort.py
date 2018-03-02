ba4b7d30b60746447935b858410ac7d55479fb87
liuxiaoguang <vincexgliu@gamil.com>
2018-03-02 10:20:46 +0800

    merge sort

--- python_fundemental/merge_sort.py
+++ python_fundemental/merge_sort.py
@@ -1,48 +1,38 @@
-
-# def mergeSort(alist):
-#     if not alist or alist is None:
-#         return
-#     else:
-#         mid = len(alist) // 2
-#         left_half = alist[:mid]
-#         right_half = alist[mid:]
-
-#         mergeSort
-
 def mergeSort(alist):
-    if len(alist) > 1:
-        mid = len(alist)//2
-        lefthalf = alist[:mid]
-        righthalf = alist[mid:]
+    print("alist---%s"%alist)
+    if len(alist) <= 1:
+        return alist
+    mid = len(alist) // 2
+    left = mergeSort(alist[:mid])
+    right = mergeSort(alist[mid:])
+    print("left---%s"%left)
+    print("right---%s"%right)
+    return merge(left, right)
 
-        mergeSort(lefthalf)
-        mergeSort(righthalf)
-
-        i = 0; j = 0; k = 0
-        while i < len(lefthalf) and j < len(righthalf):
-            if lefthalf[i] < righthalf[j]:
-                alist[k] = lefthalf[i]
-                i += 1
-            else:
-                alist[k] = righthalf[j]
-                j += 1
-            k += 1
-            print('step one: %s' % alist)
-
-        while i < len(lefthalf):
-            alist[k] = lefthalf[i]
+def merge(left, right):
+    temp = []
+    i=j=0
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            temp.append(left[i])
             i += 1
-            k += 1
-        while j < len(righthalf):
-            alist[k] = righthalf[j]
+        else:
+            temp.append(right[j])
             j += 1
-            k += 1
-        print('step two: %s' % alist)
-
+    
+    while i < len(left):
+        temp += left[i:]
+        i += 1
+    
+    while j < len(right):
+        temp += right[j:]
+        j += 1
+    print("merge--%s"%temp)
+    return temp
 
 
 
 if __name__ == '__main__':
     alist = [54,26,93,17,77,31,44,55,20]
-    mergeSort(alist)
-    print(alist)
+    a = mergeSort(alist)
+    print(a)
