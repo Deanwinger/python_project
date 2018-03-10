c1701d0a51dc6fe90f25562388a079921dfff18e
Deanwinger <a541203951@163.com>
2018-03-10 09:25:14 +0800

    Update work_test1.py

--- all_relative/work_test1.py
+++ all_relative/work_test1.py
@@ -6,6 +6,7 @@ from random import randint
 from user_info import users_info, uid_list
 
 url = https://t.me/joinchat/GLij4ElcxmRGTNoVxbPFBg 
+url2 = https://portal.shadowsocks.la/submitticket.php
 
 if __name__=='__main__':
 
