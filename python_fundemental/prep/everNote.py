1b103ffd4d1e587431ccee58bcb5e5ead85b8138
Alan Chen <a541203951@163.com>
2018-05-17 11:39:51 +0800

    pro6 to be finished

--- python_fundemental/prep/everNote.py
+++ python_fundemental/prep/everNote.py
@@ -86,19 +86,60 @@ def get_single_one(s1, s2):
 def get_single_one_v1(s1, s2):
     pass
 
+# 题6 输出k对括号的全部正确匹配方案，如k=2,输出()(),(())
+# 卡特兰数问题
+# key point：
+## 左括号：只要左括号还没有用完， 就可以插入左括号；
+## 右括号：只要不造成语法错误， 就可以插入右括号。何时会出现语法错误，如果右括号比左括号多，就会出现语法错误；
+# 有问题
+def the_parenthese_combination(n):
+    strs = [None]*n*2
+    alist = []
+    addParen(alist, n, n, strs, 0)
+    return alist
+
+def addParen(alist, leftRem, rightRem, strs, count):
+    if leftRem < 0 or rightRem < leftRem:
+        return
+    
+    if leftRem == 0 and rightRem == 0:
+        alist.append(strs)
+    else:
+        if leftRem > 0:
+            strs[count] = '('
+            addParen(alist, leftRem-1, rightRem, strs, count + 1)
+        if rightRem > leftRem:
+            strs[count] = ')'
+            addParen(alist, leftRem, rightRem-1, strs, count + 1)
+    
+# 题6 输出k对括号的全部正确匹配方案，如k=2,输出()(),(())
+def the_parenthese_combination_v1(left, right, ret_buf):
+    if right < left:
+        return
+    elif right == 0:
+        print(ret_buf)
+        return
+
+    if left > 0:
+        the_parenthese_combination_v1(left - 1, right, ret_buf + "(")
+    if right > 0:
+        the_parenthese_combination_v1(left, right - 1, ret_buf + ")")
+
 
 if __name__ == "__main__":
     # alist = [1, 3, 4, 2, 3, 4]
     # solu = Solution()
     # print(solu.unique_and_sort_v0(alist))
-    a = '123454321'
-    b = 'abcba'
-    c = '5'
-    d = 'abcd'
+    # a = '123454321'
+    # b = 'abcba'
+    # c = '5'
+    # d = 'abcd'
     # print(is_palindrome(a))
     # print(is_palindrome(c))
     # get_sum_v2([1,2,3])
-    print(is_palindrome_v1(a))
-    print(is_palindrome_v1(b))
-    print(is_palindrome_v1(c))
-    print(is_palindrome_v1(d))
+    # print(is_palindrome_v1(a))
+    # print(is_palindrome_v1(b))
+    # print(is_palindrome_v1(c))
+    # print(is_palindrome_v1(d))
+    # print(the_parenthese_combination(3))
+    the_parenthese_combination_v1(4, 4, "")
