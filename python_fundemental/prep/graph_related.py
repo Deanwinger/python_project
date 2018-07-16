44e20498cadaf829e1e8f8f02353ea91d61ffd9f
Alan Chen <a541203951@163.com>
2018-07-16 10:28:17 +0800

    graph api init

---
+++ python_fundemental/prep/graph_related.py
@@ -0,0 +1,41 @@
+
+
+class Graph:
+    def __init__(self, inputs):
+    """ create an empty graph with V vertices"""
+        pass
+
+    def Graph(self):
+    """create a graph from input stream"""
+        pass
+
+    def addEdge(self, v: int, int w):
+    """ add an edge v-w """
+        pass
+
+    def adj(self, v: int):
+    """vertices adjacent to v"""
+        pass
+
+    def nums_of_vertices(self):
+        pass
+
+    def nums_of_edges(self):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def degree(self, v: int):
+        """number of vertices that connect to v"""
+        pass
+
+    def max_degree(self):
+        pass
+
+    def average_degree(self):
+        pass
+
+    def number_of_self_loop(self):
+        pass
+    
