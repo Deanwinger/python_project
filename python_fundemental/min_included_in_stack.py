642599d412a4d7fad559a697c5ebb1840ba37e48
Alan Chen <a541203951@163.com>
2018-07-21 16:02:50 +0800

    21 题 finished

--- python_fundemental/min_included_in_stack.py
+++ python_fundemental/min_included_in_stack.py
@@ -58,3 +58,98 @@ class MinStack:
             return
         return self.submylist[-1]
         
+
+class MinStack_V1(object):
+    """使用双stack"""
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self._stack = []
+        self._min_stack = []
+        
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: void
+        """
+        self._stack.append(x)
+        if not self._min_stack:
+            self._min_stack.append(x)
+        else:
+            if self._min_stack[-1] > x:
+                self._min_stack.append(x)
+            else:
+                self._min_stack.append(self._min_stack[-1])
+
+    def pop(self):
+        """
+        :rtype: void
+        """
+        if self._stack:
+            self._stack.pop()
+            self._min_stack.pop()
+        else:
+            raise Exception("empty stack")
+        
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        if self._stack:
+            return self._stack[-1]
+        else:
+            raise Exception("empty stack")
+        
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        if self._min_stack:
+            return self._min_stack[-1]
+        else:
+            raise Exception("empty stack")
+
+class MinStack_V2(object):
+    """使用tuple"""
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self._stack = []
+        
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: void
+        """
+        curMin = self.getMin()
+        if curMin is None:
+            curMin = x
+        else:
+            if curMin > x:
+                curMin = x
+        self._stack.append((x, curMin))
+
+    def pop(self):
+        """
+        :rtype: void
+        """
+        return self._stack.pop()[0] if self._stack else None
+        
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self._stack[-1][0] if self._stack else None
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return self._stack[-1][1] if self._stack else None
