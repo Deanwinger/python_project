03e616bb06d0fc45018db159711ef87138b2dc57
Alan Chen <a541203951@163.com>
2018-07-23 17:14:27 +0800

    27 题 to be finished

--- python_fundemental/bstree_and_link_list.py
+++ python_fundemental/bstree_and_link_list.py
@@ -2,3 +2,17 @@
 输入一棵二叉搜索树，将该二叉搜索树转换成一个排序的双向链表。
 要求不能创建任何新的结点，只能调整树中结点指针的指向。
 '''
+
+# leetcode 题426， 可惜不能在线测
+# LintCode 题378
+# 题27
+
+class Solution:
+    """
+    @param: root: The root of tree
+    @return: the head of doubly list node
+    """
+    def bstToDoublyList(self, root):
+        if root is None:
+            return
+        
