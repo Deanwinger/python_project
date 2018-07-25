9a3e0a40b37237eb3a121f0294655aff416e493d
Alan Chen <a541203951@163.com>
2018-07-25 21:10:45 +0800

    mistakes operation

--- python_fundemental/first_appear.py
+++ python_fundemental/first_appear.py
@@ -2,7 +2,9 @@
 在一个字符串(1<=字符串长度<=10000，全部由大写字母组成)中找到第一个只出现一次的字符。
 '''
 
-#题 35
+#题 35 此题解答有点问题pass
+# 类似题 136. Single Number
+
 class Solution(object):
     def firstAppear(self, array):
         alphabet = {}
@@ -16,7 +18,26 @@ class Solution(object):
                 return j
         return -1
 
+class Solution:
+    def first_appear(self, alist):
+        """
+        方法2： 创建一个以ascii值为index固定大小的数组， ord()获取ascii值, chr()
+        """
+        rec = [0]*256
+
+        for s in alist:
+            rec[ord(s)] += 1
+        r = rec.index(1)
+        print(r)
+        return chr(r)
+        
+    def print_ascii(self):
+        for num in range(256):
+            print(f"{num} is {chr(num)}")
+
+
 
 if __name__=='__main__':
     S = Solution()
-    print(S.firstAppear('abaccdeff'))
+    print(S.first_appear('abbaccdeeff'))
+    s.print_ascii()
--- python_fundemental/min_number.py
+++ python_fundemental/min_number.py
@@ -2,3 +2,15 @@
 输入一个正整数数组，把数组里所有数字拼接起来排成一个数，打印能拼接出的所有数字中最小的一个。
 例如输入数组{3，32，321}，则打印出这三个数字能排成的最小数字为321323。
 '''
+
+# 题33
+
+# leetcode 179. Largest Number, 正好相反
+
+class Solution:
+    def smallestNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: str
+        """
+        
