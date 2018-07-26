bd7b60776255354e5a4a1acd6312114e2cc54a48
Alan Chen <a541203951@163.com>
2018-07-26 14:48:56 +0800

    42题to be finished

--- python_fundemental/reverse_sentence.py
+++ python_fundemental/reverse_sentence.py
@@ -13,9 +13,17 @@ class Solution(object):
         pass
 
 
-# 拓展 左旋转字符串 
+# 拓展 左旋转字符串 类似题 leetcode796. 旋转字符串
 '''
 汇编语言中有一种移位指令叫做循环左移（ROL），现在有个简单的任务，就是用字符串模拟这个指令的运算结果。
 对于一个给定的字符序列S，请你把其循环左移K位后的序列输出。
 例如，字符序列S=”abcXYZdef”,要求输出循环左移3位后的结果，即“XYZdefabc”。
 '''
+class Solution:
+    def rotateString(self, A, B):
+        """
+        :type A: str
+        :type B: str
+        :rtype: bool
+        """
+        pass
