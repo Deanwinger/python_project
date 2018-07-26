ff2f9fdf41ce7670eae2968e6ac6918b83c49595
Alan Chen <a541203951@163.com>
2018-07-26 14:36:38 +0800

    41 题 拓展to be finished

--- python_fundemental/sum_of_s.py
+++ python_fundemental/sum_of_s.py
@@ -11,6 +11,11 @@ a1    ai      am   an       aj    as
 假设恰好存在am+an=t, 小于t时, ai往前移, 大于t时, aj往后移,  
 不会出现ai移至an, aj移至am的情况
 """
+
+# 像这种存在“数组”，“查找两个数”关键字的，一律试试头尾各定义两个指针往中间扫描的思路，大多数都会有一些收获。
+
+# 题41
+
 class Solution:
     # 从左右一起查找
     # 因为当两个数的和一定的时候, 两个数字的间隔越大, 乘积越小
@@ -31,6 +36,20 @@ class Solution:
                 return [array[start], array[end]]
         return []
 
-test = [1,2,4,7,11,15]
-s = Solution()
-print(s.FindNumbersWithSum(test, 15))
+
+# leetcode 829. Consecutive Numbers Sum
+# 拓展： 和为S的连续正数序列
+class Solution:
+    def consecutiveNumbersSum(self, N):
+        """
+        :type N: int
+        :rtype: int
+        """
+        pass
+
+
+
+if __name__ == "__main__":
+    test = [1,2,4,7,11,15]
+    s = Solution()
+    print(s.FindNumbersWithSum(test, 15))
