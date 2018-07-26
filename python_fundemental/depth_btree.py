8d89d678f3093c00912e5cd38151c28574eff173
Alan Chen <a541203951@163.com>
2018-07-26 14:08:01 +0800

    39 题 finished

---
+++ python_fundemental/depth_btree.py
@@ -0,0 +1,59 @@
+# 题39 二叉树的深度  leetcode 104. Maximum Depth of Binary Tree
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if root is None:
+            return 0
+        if root.left is None and root.right is None:
+            return 1
+        
+        left = self.maxDepth(root.left)
+        right = self.maxDepth(root.right)
+        return max(left + 1, right + 1)
+
+
+# 题目2 leetcode 110. Balanced Binary Tree
+'''
+输入一棵二叉树，判断该二叉树是否是平衡二叉树。
+'''
+class Solution:
+    def isBalanced(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        if root is None:
+            return True
+        
+        left = self.maxDepth(root.left)
+        right = self.maxDepth(root.right)
+        if abs(left-right) > 1:
+            return False
+        return self.isBalanced(root.left) and self.isBalanced(root.right)
+    
+    def maxDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if root is None:
+            return 0
+        if root.left is None and root.right is None:
+            return 1
+        
+        left = self.maxDepth(root.left)
+        right = self.maxDepth(root.right)
+        return max(left + 1, right + 1)
+
