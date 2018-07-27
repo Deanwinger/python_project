17f6c2632249e2cfa904a319e7aa5f9f85f76ddc
Alan Chen <a541203951@163.com>
2018-07-27 18:49:40 +0800

    7 题 finished

--- python_fundemental/stack_and_queue.py
+++ python_fundemental/stack_and_queue.py
@@ -23,6 +23,70 @@ class queue_by_stack(object):
         return a
 
 
+# 题7
+class MyQueue:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.mystack1 = []
+        self.mystack2 = []
+        
+
+    def push(self, x):
+        """
+        Push element x to the back of queue.
+        :type x: int
+        :rtype: void
+        """
+        self.mystack1.append(x)
+        # print(self.mystack1)
+        
+
+    def pop(self):
+        """
+        Removes the element from in front of queue and returns that element.
+        :rtype: int
+        """
+        # print(self.mystack1)
+        # print(self.mystack2)
+        if self.empty():
+            print("I once been here")
+            return
+        if self.mystack2:
+            a = self.mystack2.pop()
+            print("a is ", a)
+            return a
+        else:
+            while self.mystack1:
+                self.mystack2.append(self.mystack1.pop())
+            return self.mystack2.pop()
+        
+
+    def peek(self):
+        """
+        Get the front element.
+        :rtype: int
+        """
+        if self.mystack2:
+            # print("mystack2 is ", self.mystack2)
+            return self.mystack2[-1]
+        else:
+            while self.mystack1:
+                self.mystack2.append(self.mystack1.pop())
+            return  self.mystack2[-1] if self.mystack2 else None
+
+        
+
+    def empty(self):
+        """
+        Returns whether the queue is empty.
+        :rtype: bool
+        """
+        return not self.mystack1 and not self.mystack2
+
+
 if __name__ == '__main__':
     P = queue_by_stack()
     P.push(10)
