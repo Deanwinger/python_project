4bb34e9a3384d7923e50e1fa96914e70f4059ee8
Alan Chen <a541203951@163.com>
2018-07-27 22:08:33 +0800

    27 题 finished

--- python_fundemental/bstree_and_link_list.py
+++ python_fundemental/bstree_and_link_list.py
@@ -4,15 +4,103 @@
 '''
 
 # leetcode 题426， 可惜不能在线测
+# leetcode 题114, 二叉树, 单链表
 # LintCode 题378
 # 题27
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
 
 class Solution:
     """
     @param: root: The root of tree
     @return: the head of doubly list node
     """
-    def bstToDoublyList(self, root):
+    def convert(self, root):
         if root is None:
             return
         
+        if not root.left and not root.right:
+            return root
+
+        # 处理左树
+        self.convert(root.left)
+        left = root.left
+
+        # 连接根与左子树最大节点
+        if left:
+            while left.right:
+                left = left.right
+            root.left, left.right = left, root
+        
+        # 处理右子树
+        self.convert(root.right)
+        right = root.right
+
+        if right:
+            while right.left:
+                right = right.left
+            root.right, right.left = right, root
+
+        while root.left:
+            root = root.left
+        return root
+    # def Convert(self, pRootOfTree):
+    #     if pRootOfTree == None:
+    #         return None
+    #     if not pRootOfTree.left and not pRootOfTree.right:
+    #         return pRootOfTree
+
+    #     # 处理左子树
+    #     self.Convert(pRootOfTree.left)
+    #     left = pRootOfTree.left
+
+    #     # 连接根与左子树最大结点
+    #     if left:
+    #         while left.right:
+    #             left = left.right
+    #         pRootOfTree.left, left.right = left, pRootOfTree
+
+    #     # 处理右子树
+    #     self.Convert(pRootOfTree.right)
+    #     right = pRootOfTree.right
+
+    #     # 连接根与右子树最小结点
+    #     if right:
+    #         while right.left:
+    #             right = right.left
+    #         pRootOfTree.right, right.left = right, pRootOfTree
+
+    #     while pRootOfTree.left:
+    #         pRootOfTree = pRootOfTree.left
+
+    #     return pRootOfTree
+
+if __name__=="__main__":
+    pNode1 = TreeNode(8)
+    pNode2 = TreeNode(6)
+    pNode3 = TreeNode(10)
+    pNode4 = TreeNode(5)
+    pNode5 = TreeNode(7)
+    pNode6 = TreeNode(9)
+    pNode7 = TreeNode(11)
+
+    pNode1.left = pNode2
+    pNode1.right = pNode3
+    pNode2.left = pNode4
+    pNode2.right = pNode5
+    pNode3.left = pNode6
+    pNode3.right = pNode7
+
+    S = Solution()
+    newList = S.convert(pNode1)
+    # newList = S.Convert(pNode1)
+    print(newList.val)
+
+        #             8      
+        #     6               10
+        # 5       7       9       11
