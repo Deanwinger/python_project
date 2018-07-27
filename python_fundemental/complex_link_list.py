84a628c8f14d5292c33fdadf558e23e03e8cfccd
Alan Chen <a541203951@163.com>
2018-07-28 00:26:21 +0800

    26 题 to be finished

--- python_fundemental/complex_link_list.py
+++ python_fundemental/complex_link_list.py
@@ -2,3 +2,15 @@
 输入一个复杂链表（每个节点中有节点值，以及两个指针，
 一个指向下一个节点，另一个特殊指针指向任意一个节点），返回结果为复制后复杂链表的head。
 '''
+
+# 题26
+# leetcode 138
+
+
+class Solution(object):
+    def copyRandomList(self, head):
+        """
+        :type head: RandomListNode
+        :rtype: RandomListNode
+        """
+        pass
