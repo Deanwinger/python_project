829f788d4b189e9393c964dd166a5db171a361bd
Alan Chen <a541203951@163.com>
2018-07-29 15:12:13 +0800

    34 题finished

--- python_fundemental/ugly_num.py
+++ python_fundemental/ugly_num.py
@@ -6,6 +6,7 @@
 # 题34
 
 # leetcode 263. Ugly Number
+# leetcode 264. Ugly Number II 同本题
 
 class Solution:
     def is_ugly(self, num):
@@ -13,4 +14,47 @@ class Solution:
         :type num: int
         :rtype: bool
         """
-        pass
+        # if num == 0:
+        #     return False
+
+        while num % 2 == 0:
+            num /= 2
+
+        while num % 3 == 0:
+            num /= 3
+
+        while num % 5 == 0:
+            num /= 5
+        return num == 1
+    
+    def ugly_num(self, N):
+        count = 0
+        rec = []
+        for i in range(1, n):
+            if self.is_ugly(i):
+                rec.append(i)
+                count += 1
+        print(count)
+        return rec
+
+class Solution(object):
+    def nthUglyNumber(self, n):
+        """
+        题34 的三指针解答, good one
+        :type n: int
+        :rtype: int
+        """
+        ugly = [1]
+        i2, i3, i5 = 0, 0, 0
+        while n > 1:
+            u2, u3, u5 = 2 * ugly[i2], 3 * ugly[i3], 5 * ugly[i5]
+            umin = min((u2, u3, u5))
+            if umin == u2:
+                i2 += 1
+            if umin == u3:
+                i3 += 1
+            if umin == u5:
+                i5 += 1
+            ugly.append(umin)
+            n -= 1
+        return ugly[-1]
