b306a31b2e6b28f704231bf0facd49a7d64623e9
Alan Chen <a541203951@163.com>
2018-07-29 17:33:34 +0800

    45 题finished

--- python_fundemental/Joseph_Game.py
+++ python_fundemental/Joseph_Game.py
@@ -1,3 +1,17 @@
 # 45 题
 
 # leetcode 类似题390. Elimination Game
+
+class Solution:
+    def LastRemaining_Solution(self, n, m):
+        """数学解法"""
+        if n < 1 or m < 1:
+            return -1
+        remainIndex = 0
+        for i in range(1, n+1):
+            remainIndex = (remainIndex + m) % i
+        return remainIndex
+
+    def LastRemaining(self, n, m):
+        """链表解法"""
+        pass
