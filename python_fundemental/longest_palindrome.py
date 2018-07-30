36b0f645ee95963d2584b01e6506774f35d10880
Alan Chen <a541203951@163.com>
2018-07-30 10:56:22 +0800

    51 题 init

---
+++ python_fundemental/longest_palindrome.py
@@ -0,0 +1,12 @@
+# 题 53
+# leetcode 5. Longest Palindromic Substring
+# 讲解 https://www.felix021.com/blog/read.php?2040
+
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为1000
+        :type s: str
+        :rtype: str
+        """
+        
