edbfdf226c95e54560f1b654be780b87e7c10408
Alan Chen <a541203951@163.com>
2018-07-30 17:02:00 +0800

    54 题 finished

---
+++ python_fundemental/54_Kth_Largest_Element.py
@@ -0,0 +1,98 @@
+# leetcode 703. Kth Largest Element in a Stream
+
+
+# 据说堆序对于最大Kth element 相关题目很好使, 但是随便写写就小100行了
+
+
+class KthLargest:
+
+    def __init__(self, k, nums):
+        """
+        :type k: int
+        :type nums: List[int]
+        """
+        length = len(nums)
+        self.heap = PrioQueue(nums)
+        self.kth_num = self.get_kth_num(length-k) if length-k>0 else None
+        self.limit = k
+        
+    def get_kth_num(self, m):
+        while m:
+            self.heap.dequeue()
+            m -= 1
+        return self.heap.peek()
+
+    def add(self, val):
+        """
+        :type val: int
+        :rtype: int
+        """
+        self.heap.enqueue(val)
+        
+        if len(self.heap) < self.limit:
+            return
+        if len(self.heap) > self.limit:
+            self.heap.dequeue()
+
+        self.kth_num = self.heap.peek()
+        return self.kth_num
+
+# 基于（小顶）堆的优先队列
+class PrioQueue:
+    """ Implementing priority queues using heaps
+    """
+    def __init__(self, elist=[]):
+        self._elems = list(elist)
+        if elist:
+            self.buildheap()
+
+    def is_empty(self):
+        return not self._elems
+
+    def peek(self):
+        if self.is_empty():
+            # raise PrioQueueError("in peek")
+            return
+        return self._elems[0]
+    
+    def enqueue(self, e):
+        self._elems.append(None)  # add a dummy element
+        self.siftup(e, len(self._elems)-1)
+
+    def siftup(self, e, last):
+        elems, i, j = self._elems, last, (last-1)//2
+        while i > 0 and e < elems[j]:
+            elems[i] = elems[j]
+            i, j = j, (j-1)//2
+        elems[i] = e
+
+    def dequeue(self):
+        if self.is_empty():
+            # raise PrioQueueError("in dequeue")
+            return None
+        elems = self._elems
+        e0 = elems[0]
+        e = elems.pop()
+        if len(elems) > 0:
+            self.siftdown(e, 0, len(elems))
+        return e0
+
+    def siftdown(self, e, begin, end):
+        elems, i, j = self._elems, begin, begin*2+1
+        while j < end:    # invariant: j == 2*i+1
+            if j+1 < end and elems[j+1] < elems[j]:
+                j += 1    # elems[j] <= its brother
+            if e < elems[j]:     # e is the smallest of the three
+                break
+            elems[i] = elems[j]  # elems[j] is the smallest, move it up
+            i, j = j, 2*j+1
+        elems[i] = e
+
+    def buildheap(self):
+        end = len(self._elems)
+        for i in range(end//2, -1, -1):
+            self.siftdown(self._elems[i], i, end)
+        
+    def __len__(self):
+        return len(self._elems)
+    
