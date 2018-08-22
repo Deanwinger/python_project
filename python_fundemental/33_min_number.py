6b587ef89f4e6bfc7493c1b415fa2d54cad70d76
Alan Chen <a541203951@163.com>
2018-08-22 10:33:24 +0800

    33 题 to be finished

--- python_fundemental/33_min_number.py
+++ python_fundemental/33_min_number.py
@@ -14,3 +14,43 @@ class Solution:
         :rtype: str
         """
         
+
+from functools import reduce
+
+
+# leetcode 179, 答案错误， 待调试
+class Solution:
+    def largestNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: str
+        """
+        rec = self.permute(nums)
+        return reduce(self.compare, rec)
+        
+    def permute(self, nums):
+        if len(nums) == 1:
+            return str(nums[0])
+        
+        rec = []
+        n = len(nums)
+        for i in range(n):
+            res = self.permute(nums[:i]+nums[i+1:])
+            for r in res:
+                rec.append(str(nums[i])+r)
+        return rec
+    
+    def compare(self, m, n):
+        s = len(m)
+        for i in range(s):
+            if m[i] > n[i]:
+                return m
+            elif n[i] > m[i]:
+                return  n
+            else:
+                i += 1
+
+if __name__ == "__main__":
+    nums = [3,30,34,5,9]
+    s = Solution()
+    print(s.largestNumber(nums))
