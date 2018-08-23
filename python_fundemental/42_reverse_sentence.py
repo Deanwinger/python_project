d14e55edbbfb28d5738bea580d72ff74aa8775e5
Alan Chen <a541203951@163.com>
2018-08-23 21:29:16 +0800

    42 题 finished

--- python_fundemental/42_reverse_sentence.py
+++ python_fundemental/42_reverse_sentence.py
@@ -4,32 +4,7 @@
 # 题42
 
 
-class Solution(object):
-    def reverseWords(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
-        pass
-
-
-# 拓展 左旋转字符串 类似题 leetcode796. 旋转字符串
-'''
-汇编语言中有一种移位指令叫做循环左移（ROL），现在有个简单的任务，就是用字符串模拟这个指令的运算结果。
-对于一个给定的字符序列S，请你把其循环左移K位后的序列输出。
-例如，字符序列S=”abcXYZdef”,要求输出循环左移3位后的结果，即“XYZdefabc”。
-'''
-class Solution:
-    def rotateString(self, A, B):
-        """
-        :type A: str
-        :type B: str
-        :rtype: bool
-        """
-        pass
-
-
-class Solution(object):
+class Solu(object):
     def reverseWords(self, s):
         """
         :type s: str
@@ -60,9 +35,48 @@ class Solution(object):
         s = s[start:end]
         return s[::-1]
 
+    def reverseWords_v1(self, s):
+        """
+        解法2: 及其简单的解
+        :type s: str
+        :rtype: str
+        """
+        if not s:
+            return ""
+        
+        s = s.split()
+        s = s[::-1]
+        return " ".join(s)
+            
+
+
+# 拓展 左旋转字符串 类似题 leetcode796. 旋转字符串
+class Solution:
+    def rotateString(self, A, B):
+        """
+        :type A: str
+        :type B: str
+        :rtype: bool
+        """
+        if len(A) != len(B):
+            return False
+        
+        if A == "" and B == "":
+            return True
+        
+        n = len(A)
+        for i in range(n):
+            if A[i] == B[0]:
+                print(A[i])
+                t = A[i:]+A[:i]
+                if t == B:
+                    return True
+        return False
+
+
 if __name__ == "__main__":
     test = "the sky is blue"
-    s = Solution()
+    s = Solu()
     # print(s.FindNumbersWithSum(test, 15))
-    print(s.reverseWords(test))
+    print(s.reverseWords_v1(test))
 
