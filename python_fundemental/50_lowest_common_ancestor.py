f4f516d34d01ff03c487b253f00c955967cce26a
Alan Chen <a541203951@163.com>
2018-08-25 15:46:12 +0800

    50 题 finished

--- python_fundemental/50_lowest_common_ancestor.py
+++ python_fundemental/50_lowest_common_ancestor.py
@@ -35,3 +35,37 @@ class Solution:
         if not found:
             rec.pop()
         return found
+
+# 8.25 重做, 发现基本上就是25题的思路即可, 对于这种找存在的递归, 值得好好反复思考;
+class Solut(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        rec1 = []
+        rec2 = []
+        self.find_path(root, p, rec1)        
+        self.find_path(root, q, rec2)
+        return [r for r in rec1 if r in rec2][-1]
+        
+    def find_path(self, root, node, rec):
+        if not root:
+            return False
+        
+        rec.append(root)
+        found = False
+                
+        if root is node:
+            return True
+        
+        found = self.find_path(root.left, node, rec)
+        if not found:
+            found = self.find_path(root.right, node, rec)
+            
+        if not found:
+            rec.pop()
+            
+        return found
