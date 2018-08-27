e25c59e7208009f59006e65250bc8dc758aaae3b
Alan Chen <a541203951@163.com>
2018-08-27 18:05:45 +0800

    94 题 to be finished

--- python_fundemental/94_list_node_add.py
+++ python_fundemental/94_list_node_add.py
@@ -35,6 +35,82 @@ class Solution:
     
     def addTwoNumbers_v1(self, l1, l2):
         """
+        8.27 日再次尝试, 发现还是写成了一坨屎, 未通过, 卒;
         解法2: 逆序, 然后逐个想加, to be finished
         """
-        pass
+        if not l1:
+            return l2
+        
+        if not l2:
+            return l1
+        # 第一步, 逆序
+        head1, m = self.reverse_llist(l1)
+        head2, n = self.reverse_llist(l2)
+
+        dummy = head =  ListNode(hea1.val+head2.val)
+
+        flag = False
+        steps = min(m, n)
+        while steps:
+            val = hea1.val + head2.val
+            if flag:
+                val += 1
+                if val >= 10:
+                    val -= 10
+                    flag = True
+                else:
+                    flag = False
+            head.next = ListNode(val)
+            head = head.next
+            head1 = head1.next
+            head2 = head2.next
+            steps -= 1
+        
+        while head1:
+            val = head1.val
+            if flag:
+                val += 1
+                if val >= 10:
+                    val -= 10
+                    flag = True
+                else:
+                    flag = False
+            head.next = ListNode(val)
+            head = head.next
+            head1 = head1.next
+
+        while head2:
+            val = head2.val
+            if flag:
+                val += 1
+                if val >= 10:
+                    val -= 10
+                    flag = True
+                else:
+                    flag = False
+            head.next = ListNode(val)
+            head = head.next
+            head2 = head2.next            
+        
+        h = self.reverse_llist(self, dummy)
+        return h
+
+        
+
+
+    def reverse_llist(self, root):
+        head = root
+        root = root.next
+        head.next = None
+        count = 1
+        while root:
+            tem = root.next
+            root.next = head
+            head = root
+            root = tem
+            count += 1
+        return root, count
+        
+
+
+        
