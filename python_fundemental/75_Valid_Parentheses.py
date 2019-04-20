d41c9260d9713af3bf3841018c05b8bb5d51aee9
Alan Chen <a541203951@163.com>
2019-04-20 17:21:07 +0800

    75 finished

--- python_fundemental/75_Valid_Parentheses.py
+++ python_fundemental/75_Valid_Parentheses.py
@@ -1,1 +1,26 @@
 # leetcode 20 Valid Parentheses
+
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        if not s:
+            return True
+        
+        stack = []
+        n = len(s)
+        i = 1
+        stack.append(s[0])
+        while i < n:
+            if stack and self.is_pair(stack[-1], s[i]):
+                stack.pop()
+            else:
+                stack.append(s[i])
+            i += 1
+            
+        if len(stack) == 0:
+            return True
+        else:
+            return False
+        
+    def is_pair(self, left, right):
+        return (left=='(' and right ==')') or (left=='[' and right ==']') or (left=='{' and right =='}')
