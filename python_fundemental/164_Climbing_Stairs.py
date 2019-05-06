d38fa374c3ab3c10a3480b4252ac992c27c53a7f
Alan Chen <a541203951@163.com>
2019-05-06 16:42:17 +0800

    leetcode 164 done

---
+++ python_fundemental/164_Climbing_Stairs.py
@@ -0,0 +1,23 @@
+# leetcode 70. Climbing Stairs (Easy)
+
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        '''最直观的解法, 但是会超出时间限制'''
+        if n == 1 :
+            return 1
+        if n == 2:
+            return 2
+        return self.climbStairs(n-1) + self.climbStairs(n-2)
+
+    def climbStairs_v1(self, n: int) -> int:
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        starter = [1,2]
+        i = 3
+        while i <= n:
+            starter[(i-1)%2] = starter[0] + starter[1]
+            i += 1
+        return starter[(n-1)%2]
