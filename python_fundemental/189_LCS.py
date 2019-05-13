93d00179e81f8c1a3047797a517e239a4a165552
Alan Chen <a541203951@163.com>
2019-05-13 13:06:52 +0800

    189  add comment

--- python_fundemental/189_LCS.py
+++ python_fundemental/189_LCS.py
@@ -1,5 +1,9 @@
-#  最长公共子串(LCS)
+#  最长公共子序列(LCS)
 # leetcode 583. 两个字符串的删除操作 基本上就是实现下面的LCS
+
+#  最长公共子串（Longest Common Substring）与最长公共子序列（Longest Common Subsequence）的区别： 
+# 子串要求在原字符串中是连续的，而子序列则只需保持相对顺序一致，并不要求连续。
+# 例如X = {a, Q, 1, 1}; Y = {a, 1, 1, d, f}那么，{a, 1, 1}是X和Y的最长公共子序列，但不是它们的最长公共字串。
 """
 public int lengthOfLCS(int[] nums1, int[] nums2) {
     int n1 = nums1.length, n2 = nums2.length;
@@ -20,8 +24,8 @@ public int lengthOfLCS(int[] nums1, int[] nums2) {
 
 # 这只不过是计算出长度, 如何打印出LCS, to be finished
 def LCS(s1, s2):
-    n1 = len(s1)
-    n2 = len(s2)
+    n1 = len(s1) # 对应行
+    n2 = len(s2) # 对应列
 
     # 我靠, []*n 这个操作是浅拷贝
     # dp=[[0]*(n2+1)]*(n1+1)
