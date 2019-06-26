9b8abf622243cbaa2c925105101e6214cf960c4b
Alan Chen <a541203951@163.com>
2019-06-26 17:59:02 +0800

    223 done

--- python_fundemental/223_merge_tree.py
+++ python_fundemental/223_merge_tree.py
@@ -23,3 +23,30 @@ class Solution:
         t1.left = self.mergeTrees(t1.left, t2.left)
         t1.right = self.mergeTrees(t1.right, t2.right)
         return t1
+
+
+# leetcode 111. 二叉树的最小深度
+# 2019-6-26 迭代法 to be finished...
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def minDepth(self, root: TreeNode) -> int:
+        if not root:
+            return 0
+        if root.left is None and root.right is None:
+            return 1
+        
+        if root.left and root.right:
+            left = self.minDepth(root.left)
+            right = self.minDepth(root.right)
+            return min(left, right)+1
+        if root.left:
+            return self.minDepth(root.left)+1
+        if root.right:
+            return self.minDepth(root.right)+1
