cde8849eb13cabaf662edcca19cf5be56acd05b4
Alan Chen <a541203951@163.com>
2019-07-14 12:48:47 +0800

    6 done

--- python_fundemental/6_btree_rebuild.py
+++ python_fundemental/6_btree_rebuild.py
@@ -3,11 +3,13 @@
 假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
 例如输入前序遍历序列{1,2,4,7,3,5,6,8}和中序遍历序列{4,7,2,1,5,3,8,6}，则重建二叉树并返回。
 '''
-# leetcode 105
-#关键在于递归遍历， 关键点就在于找到根节点， 然后对于每个子树，都用找根节点确定左右子树的方式递归的进行下去
+# leetcode 105 -- 剑指offer 题 6
+# leetcode 106. 从中序与后序遍历序列构造二叉树
+# leetcode 889. 根据前序和后序遍历构造二叉树
+# 关键在于递归遍历， 关键点就在于找到根节点， 然后对于每个子树，都用找根节点确定左右子树的方式递归的进行下去
 # 2019.2.13 
+# 2019.4.9 
 
-# 题 6
 
 class Node(object):
     def __init__(self, value):
@@ -41,6 +43,8 @@ class Solution:
         root.left = self.reConstructBinaryTree(pre[1:i+1], tin[:i])
         root.right = self.reConstructBinaryTree(pre[i+1:], tin[i+1:])
         return root
+
+
 
     
 if __name__ == '__main__':
