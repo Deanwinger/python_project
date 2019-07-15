9e4fff15680322385a374c3472ae2bacb5200a39
Alan Chen <a541203951@163.com>
2019-07-15 15:05:12 +0800

    242 done

--- python_fundemental/242_Group_Anagrams.py
+++ python_fundemental/242_Group_Anagrams.py
@@ -1,2 +1,24 @@
 # leetcode 49. Group Anagrams
-# 程序员代码面试指南 p240
+# 程序员代码面试指南 P242
+
+
+class Solution:
+    def groupAnagrams(self, strs):
+        rec = {}
+        for s in strs:
+            r = [ord(i) for i in s]
+            r.sort()
+            r= tuple(r)
+            if rec.get(r) is None:
+                rec[r] = [s]
+            else:
+                rec[r].append(s)
+            
+        res = [r for r in rec.values()]
+        return res
+
+
+if __name__ =='__main__':
+    t = ["eat","tea","tan","ate","nat","bat","eat"]
+    s = Solution()
+    s.groupAnagrams(t)
